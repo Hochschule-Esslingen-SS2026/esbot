3b737122eac562b3c6089eeab00541984022a6c4
lekiit01 <lekiit01@hs-esslingen.de>
2026-04-18 19:12:08 +0200

    RequestQuiz Test Steps

--- EsBot-Server/Core.Data/DTOs/Requests/CreateQuizRequest.cs
+++ EsBot-Server/Core.Data/DTOs/Requests/CreateQuizRequest.cs
@@ -2,5 +2,7 @@ namespace Core.Data.DTOs.Requests;
 
 public record CreateQuizRequest
 {
-    
+    public required Guid UserSessionId { get; set; }
+
+    public required string Topic { get; set; }
 }
--- EsBot-Server/Core.Interfaces/Services/IQuizManagementService.cs
+++ EsBot-Server/Core.Interfaces/Services/IQuizManagementService.cs
@@ -1,6 +1,9 @@
+using Core.Data.DTOs.Requests;
+using Core.Data.DTOs.Responses;
+
 namespace Core.Interfaces.Services;
 
 public interface IQuizManagementService
 {
-    
+    Task<QuizRequestResponse> RequestQuiz(CreateQuizRequest request);
 }
